68e47090c702a79a7ac32424e24ed3e1a93876d6
Tomoki-0526 <eusford_0526@outlook.com>
2024-10-28 16:53:49 +0800

    hpn

--- utils/tf_logger.py
+++ utils/tf_logger.py
@@ -12,17 +12,17 @@ except ImportError:
 class Logger(object):
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        # self.writer = tf.summary.FileWriter(log_dir)
-        self.writer = tf.summary.create_file_writer(log_dir)
+        self.writer = tf.summary.FileWriter(log_dir)    # env=hpn
+        # self.writer = tf.summary.create_file_writer(log_dir)  # env=none
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        # summary = tf.Summary(
-        #     value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        # self.writer.add_summary(summary, step)
-        with self.writer.as_default():
-            tf.summary.scalar(tag, value, step=step)
-            self.writer.flush()
+        summary = tf.Summary(
+            value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        self.writer.add_summary(summary, step)  # env=hpn
+        # with self.writer.as_default():
+        #     tf.summary.scalar(tag, value, step=step)
+        #     self.writer.flush()   # env=none
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
